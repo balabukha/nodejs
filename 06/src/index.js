8c50d8c7c625585c00170ce27e211afed326a204
balabukha <3909591@gmail.com>
2018-01-07 09:08:47 +0300

    dev-06-02

--- 06/src/index.js
+++ 06/src/index.js
@@ -1,32 +1,76 @@
 // 06 MongoDb
-// import express from 'express';
+
+import express from 'express';
 // import fetch from 'isomorphic-fetch';
 // import _ from 'lodash';
-import Promise from 'bluebird';
+// import Promise from 'bluebird';
 import mongoose from 'mongoose';
 
-mongoose.Promise = Promise;
-const dbURL = 'mongodb://balabukha:86yerdna@ds245687.mlab.com:45687/balabukah';
+import Pet from './models/Pet';
+import User from './models/User';
+import saveDataInDb from './saveDataInDb';
 
+// mongoose.Promise = Promise;
+const dbURL = 'mongodb://balabukha:86yerdna@ds245687.mlab.com:45687/balabukah';
 mongoose.connect(dbURL);
 
-const Pet = mongoose.model('Pet', {
-  type: String,
-  name: String,
+const app = express(); // run express
+
+app.get('/users', async (req, res) => {
+  const users = await User.find();
+  res.send(users);
 });
 
-const kitty = new Pet({
-  name: 'Nutells',
-  type: 'cat',
+app.get('/pets', async (req, res) => {
+  const pets = await Pet.find();
+  res.send(pets);
 });
 
-kitty.save()
-.then(() => {
-  console.log('OK');
-})
-.catch((err) => {
-  console.log('ERR', err);
+app.post('/data', async (req, res) => {
+  const data = req.body;
+  saveDataInDb(data);
+  res.send(data);
 });
+
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!');
+});
+
+// const data = {
+//   user: {
+//     name: 'Andrey',
+//   },
+//   pets: [
+//     {
+//       name: 'Nutella',
+//       type: 'cat',
+//     },
+//     {
+//       name: 'Motya',
+//       type: 'shinshilla',
+//     },
+//   ],
+// };
+
+
+// const Pet = mongoose.model('Pet', {
+//   type: String,
+//   name: String,
+// });
+
+// const kitty = new Pet({
+//   name: 'Nutells',
+//   type: 'cat',
+// });
+
+
+// kitty.save()
+// .then(() => {
+//   console.log('OK');
+// })
+// .catch((err) => {
+//   console.log('ERR', err);
+// });
 
 // const app = express(); // run express
 
